2d7eed53532958dce69fb696e68120ff40f57348
Emerson <lyncoln_erc@hotmail.com>
2024-02-12 14:16:41 -0300

    feat: att dropDown

--- src/Screens/Cotacoes/Cotacoes.tsx
+++ src/Screens/Cotacoes/Cotacoes.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { View, Text, FlatList, TouchableOpacity, Modal } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, Modal, SectionList, } from 'react-native';
 import { useClientesContext } from '../../Context/useClienteContext';
 import { ModalCliente } from '../../Modal/ModalCliente';
+import { Picker } from '@react-native-picker/picker';
 
 import { styles } from './CotacoesStyles';
 
@@ -11,15 +12,67 @@ interface Cliente {
     Modelo?: string,
     Vistoria?: string,
     "Valor Protegido"?: string,
+    DiaCadastro?: number,
+    MesCadastro?: number,
+    AnoCadastro?: number,
 }
+
+
 
 export function Cotacoes() {
 
     const { clientes, removerCliente, selecionarCliente } = useClientesContext();
 
     const [modalVisible, setModalVisible] = useState(false);
+    const [selectedMonth, setSelectedMonth] = useState(1);
+    const [selectedDay, setSelectedDay] = useState(1);
+    const [clientesFiltrados, setClientesFiltrados] = useState<Cliente[]>((clientes));
 
-    const renderItem = ({ item, index }: { item: Cliente; index: number }) => {
+    const meses = [
+        { label: 'Mostrar todos', value: '0' },
+        { label: 'Janeiro', value: '1' },
+        { label: 'Fevereiro', value: '2' },
+        { label: 'Março', value: '3' },
+        { label: 'Abril', value: '4' },
+        { label: 'Maio', value: '5' },
+        { label: 'Junho', value: '6' },
+        { label: 'Julho', value: '7' },
+        { label: 'Agosto', value: '8' },
+        { label: 'Setembro', value: '9' },
+        { label: 'Outubro', value: '10' },
+        { label: 'Novembro', value: '11' },
+        { label: 'Dezembro', value: '12' },
+    ];
+
+
+    const dias = [
+        { label: 'Mostrar todos', value: '0' },
+        ...Array.from({ length: 31 }, (_, index) => ({ label: `${index + 1}`, value: `${index + 1}` })),
+    ];
+
+
+    const filtrarClientes = () => {
+        if (selectedMonth == 0 && selectedDay == 0) {
+            setClientesFiltrados(clientes);
+        }
+
+        if (selectedMonth != 0 && selectedDay != 0) {
+            const clientesFiltradosDiaMes = clientes.filter(
+                cliente => cliente.DiaCadastro == selectedDay && cliente.MesCadastro == selectedMonth
+            );
+            setClientesFiltrados(clientesFiltradosDiaMes);
+        }
+
+        if (selectedMonth != 0 && selectedDay == 0) {
+            const clientesFiltradosMes = clientes.filter(cliente => cliente.MesCadastro == selectedMonth);
+            setClientesFiltrados(clientesFiltradosMes);
+        }
+    };
+
+
+
+
+    const renderItem = ({ item, index }: { item: Cliente, index: number }) => {
         return (
             <TouchableOpacity
                 onPress={() => {
@@ -34,30 +87,77 @@ export function Cotacoes() {
                     </View>
 
                     <View style={{ flexDirection: "row" }}>
-                        <Text style={{ width: "20%" }}>Carro: </Text>< Text style={styles.text}>{item.Modelo}</Text>
+                        {/*  <Text style={{ width: "20%" }}>Carro: </Text>< Text style={styles.text}>{item.Modelo}</Text> */}
+                        <Text style={{ width: "20%" }}>Data: </Text>
+                        < Text style={styles.text}>{item['DiaCadastro']}/{item['MesCadastro']}/{item['AnoCadastro']}</Text>
                     </View>
                 </View>
 
                 <View style={{ width: "100%" }} >
                     <Text style={{ width: "35%", textAlign: "center" }}>Telefone:</Text><Text style={[styles.text, { textAlign: "center", width: "33%" }]}>{item.Telefone}</Text>
                 </View>
+
             </TouchableOpacity>
+
         );
     };
 
+
+
     return (
+
+
         <View style={styles.container}>
             <Text style={styles.title}>Cotações Salvas</Text>
+
+
+
+
+            <View style={styles.dropContainer}>
+                <Text style={{ color: "#fff", fontSize: 20 }}>Selecione o mês</Text>
+                <View style={styles.picker}>
+                    <Picker
+                        selectedValue={selectedMonth}
+                        onValueChange={(itemValue, itemIndex) => setSelectedMonth(itemValue)}
+                    >
+                        {meses.map((mes) => (
+                            <Picker.Item key={mes.value} label={mes.label} value={mes.value} />
+                        ))}
+                    </Picker>
+                </View>
+            </View>
+            <View style={styles.dropContainer}>
+                <Text style={{ color: "#fff", fontSize: 20 }}>Selecione o dia</Text>
+                <View style={styles.picker}>
+
+                    <Picker
+                        selectedValue={selectedDay}
+                        onValueChange={(itemValue, itemIndex) => setSelectedDay(itemValue)}
+                    >
+                        {dias.map((dia) => (
+                            <Picker.Item key={dia.value} label={dia.label} value={dia.value} />
+                        ))}
+                    </Picker>
+                </View>
+            </View>
+
             <View style={styles.listContainer}>
-                {clientes.length > 0 ? (<FlatList
-                    data={clientes}
+                <FlatList
+                    data={clientesFiltrados}
                     renderItem={renderItem}
                     keyExtractor={(item, index) => index.toString()}
-                />) : (<Text style={styles.textoVazio}>Nenhuma Cotação Salva</Text>)
-                }
-
+                />
 
             </View>
+
+            <TouchableOpacity
+                //onPress={() => alert(JSON.stringify(clientes))}
+                onPress={() => filtrarClientes()}
+
+                style={{ backgroundColor: "#fff" }}
+            >
+                <Text>Clientes Filtrados</Text>
+            </TouchableOpacity>
 
             <Modal
                 visible={modalVisible}
